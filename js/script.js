e5d542e1d2c060414b38756c8d2eadae2be5f664
Save-brt <139074778+Save-brt@users.noreply.github.com>
2024-05-03 14:35:07 +0200

    Misc

--- js/script.js
+++ js/script.js
@@ -35,17 +35,18 @@ fetch('https://dummyjson.com/products?limit=100')
             </div>
         </div>
         `
-
-        let aaa = document.getElementById('aa');
-        aaa.innerHTML = `<button product-id="${prodotto.id}" class="btn-aggiungi-carrello">Aggiungi al carrello<span class="fa-solid fa-cart-plus"></span></button>`
-
-        let btnAggiungiAlCarrello = document.querySelector(`.btn-aggiungi-carrello[product-id="${prodotto.id}"]`);
-
-        btnAggiungiAlCarrello.addEventListener('click',function() {
-                aggiungiAlCarrello(prodotto.id);
-            });
         
     });
+    
+    let allBtnAggiungiAlCarrello = document.querySelectorAll(`.btn-aggiungi-carrello`);
+    [...allBtnAggiungiAlCarrello].forEach(btn => {
+        btn.addEventListener('click',function(){
+            aggiungiAlCarrello(this.getAttribute('product-id'));
+        });
+    });
+
+
+
 
 }
 
@@ -57,46 +58,46 @@ function aggiungiAlCarrello(prodottoId) {
 
     let divProdottiCarrello = document.getElementById('divProdottiCarrello');
 
-    divProdottiCarrello.innerHTML = 
+    divProdottiCarrello.innerHTML += 
     `
     <div class="elProdottoCarrello">
-      <img ${arrayProducts[prodottoId - 1].images[0]} alt="foto prodotto">
-      <i>-</i>
-      <input type="number" name="" id="" maxlength="2" min="1" value="1">
-      <i>+</i>
-      <p>${arrayProducts[prodottoId - 1].price},99€</p>
+        <div class="carrelloNomeEImgProdotto">
+            <img src="${arrayProducts[prodottoId - 1].images[0]}" alt="foto prodotto">
+            <p>${arrayProducts[prodottoId - 1].title}</p>
+        </div>
+
+        <div class="carrelloQuantita">
+            <span class="fa-solid fa-minus decreaseQuantity"></span>
+            <input type="number" class="quantityInput" max="99" min="1" value="1">
+            <span class="fa-solid fa-plus increaseQuantity"></span>
+        </div>
+        
+        <p>${arrayProducts[prodottoId - 1].price},99€</p>
     </div>
     <hr>
-    `
+    `;
+
+    divProdottiCarrello.addEventListener('click', function(event) {
+        const target = event.target;
+
+        if (target.classList.contains('decreaseQuantity')) {
+            const input = target.nextElementSibling; // troverà l'input
+            if (input.value > 1) {
+                input.value = parseInt(input.value) - 1;
+                target.style.color = "black"; // ripristina il colore
+            } else {
+                target.style.color = "gray"; // se il valore è 1 o inferiore
+            }
+        }
+
+        if (target.classList.contains('increaseQuantity')) {
+            const input = target.previousElementSibling; // troverà l'input
+            if (input.value < 99) {
+                input.value = parseInt(input.value) + 1;
+                target.style.color = "black"; // ripristina il colore
+            } else {
+                target.style.color = "gray"; // se il valore è 99 o superiore
+            }
+        }
+    });
 }
-
-
-
-
-
-
-
-
-/* categorie
-[
-"smartphones",
-"laptops",
-"fragrances",
-"skincare",
-"groceries",
-"home-decoration",
-"furniture",
-"tops",
-"womens-dresses",
-"womens-shoes",
-"mens-shirts",
-"mens-shoes",
-"mens-watches",
-"womens-watches",
-"womens-bags",
-"womens-jewellery",
-"sunglasses",
-"automotive",
-"motorcycle",
-"lighting"
-] */
